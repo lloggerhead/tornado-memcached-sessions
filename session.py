50fc89d61d4f9617ef4ed4e71f1bf08f3ff373e0
Fangzheng Long <lloggerhead@gmail.com>
2013-11-19 13:22:13 +0800

    Update session.py

--- session.py
+++ session.py
@@ -7,13 +7,13 @@ import memcache
 class Session(dict):
     _registered = False
 
-    class SessionNotRegisterException(Exception):
+    class SessionNotRegisterError(Exception):
         pass
-    class InvalidSessionException(Exception):
+    class SessionInvalidError(Exception):
         pass
-    class ConnectMemcachedServerException(Exception):
+    class ConnectMemcachedServerError(Exception):
         pass
-    
+
     @classmethod
     def register(cls, secret, memcached_address=["127.0.0.1:11211"], expires_days=7):
         cls.secret = secret
@@ -23,24 +23,21 @@ class Session(dict):
 
     def __init__(self, request_handler):
         if not Session._registered:
-            raise SessionNotRegisterException()
-        self.request_handler = request_handler
-        self.ssid = request_handler.get_secure_cookie("ssid")
-        self.verf = request_handler.get_secure_cookie("verf")
-        self._check_ssid()
-        self.load()
+            raise Session.SessionNotRegisterError()
+        self.ssid, self.verf = self._get_ssid_and_verf(request_handler)
+        data = self.load()
+        for key, value in data.items():
+            self[key] = value
 
     # read datas from memcached server
     def load(self):
-        mc = memcache.Client(Session.memcached_address)
-        self._check_memclient(mc)
+        mc = self._get_memclient()
         data = mc.get(self.ssid)
         if data:
             data = pickle.loads(data)
         else:
             data = dict()
-        for key, value in data.items():
-            self[key] = value
+        return data
 
     # save datas to memcached server
     def save(self):
@@ -48,32 +45,36 @@ class Session(dict):
         data = dict(self.items())
         data = pickle.dumps(data, pickle.HIGHEST_PROTOCOL)
         timeout = Session.expires_days * 24 * 60 * 60
-        mc = memcache.Client(Session.memcached_address)
-        self._check_memclient(mc)
+        mc = self._get_memclient()
         mc.set(self.ssid, data, timeout)
 
     def clear(self):
         super(Session, self).clear()
-        self.request_handler.clear_cookie("ssid")
-        self.request_handler.clear_cookie("verf")
+        mc = self._get_memclient()
+        mc.delete(self.ssid)
+
+    def _get_memclient(self):
+        mc = memcache.Client(Session.memcached_address)
+        if not mc.get_stats():
+            raise Session.ConnectMemcachedServerError()
+        return mc
+
+    def _get_ssid_and_verf(self, request_handler):
+        ssid = request_handler.get_secure_cookie("ssid")
+        verf = request_handler.get_secure_cookie("verf")
+        if not (ssid and verf):
+            ssid = self._generate_ssid()
+            verf = self._generate_verf(ssid)
+            request_handler.set_secure_cookie("ssid", ssid, Session.expires_days)
+            request_handler.set_secure_cookie("verf", verf, Session.expires_days)
+        elif verf != self._generate_verf(ssid):
+            request_handler.clear_cookie("ssid")
+            request_handler.clear_cookie("verf")
+            raise Session.SessionInvalidError()
+        return ssid, verf
 
     def _generate_ssid(self):
         return hashlib.sha256(Session.secret + str(uuid.uuid4())).hexdigest()
 
     def _generate_verf(self, ssid):
         return hmac.new(ssid, Session.secret, hashlib.sha256).hexdigest()
-
-    def _check_ssid(self):
-        if not (self.ssid and self.verf):
-            self.ssid = self._generate_ssid()
-            self.verf = self._generate_verf(self.ssid)
-            self.request_handler.set_secure_cookie("ssid", self.ssid, self.expires_days)
-            self.request_handler.set_secure_cookie("verf", self.verf, self.expires_days)
-        elif self.verf != self._generate_verf(self.ssid):
-            self.request_handler.clear_cookie("ssid")
-            self.request_handler.clear_cookie("verf")
-            raise InvalidSessionException()
-
-    def _check_memclient(self, mc):
-        if not mc.get_stats():
-            raise ConnectMemcachedServerException()
